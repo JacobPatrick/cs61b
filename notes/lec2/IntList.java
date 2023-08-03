ecb668bde3fae60efb221950282843d23fd802a5
Jacob Patrick <Jacob_Patrick@163.com>
2023-08-03 23:41:51 +0800

    20230823_update

---
+++ notes/lec2/IntList.java
@@ -0,0 +1,21 @@
+public class IntList {
+    public int first;
+    public IntList rest;
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    /** Return the size of this IntList. */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    /** Return the i th element of this IntList. */
+    public int get(int i) {
+        return 1;
+    }
+}
